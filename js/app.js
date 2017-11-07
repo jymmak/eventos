5a62719bca961f358d94b5206d9af43eb31db23e
jymmak <henryfrancisco_fernando@hotmail.com>
2017-11-07 11:39:15 -0500

    eventos

---
+++ js/app.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    var board = document.querySelector('.board-js');
+    
+    board.addEventListener('click', addX);
+};
+
+var centinel = true ;
+function addX(event) {
+    if (centinel)
+      event.target.textContent = 'X';
+      else
+         event.target.textContent = 'O';
+         centinel=!centinel;
+ 
+} 
